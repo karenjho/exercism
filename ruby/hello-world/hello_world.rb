2f450a09e68ac83fc025903a6f3b66089f62319b
karenjho <hokarenj@gmail.com>
2016-03-31 16:49:38 -0400

    Add comments to hello_world

--- ruby/hello-world/hello_world.rb
+++ ruby/hello-world/hello_world.rb
@@ -1,9 +1,15 @@
 class HelloWorld
 
+  # Define 'hello' as a class method
+  # Accept optional arguments (i.e. a name)
   def self.hello(*args)
+    # If no name is given...
     if args == []
+      # Return "Hello, World!"
       "Hello, World!"
+    # If a name is given...
     else
+      # Return "Hello, [name]!"
       "Hello, " + args[0] +"!"
     end
   end
